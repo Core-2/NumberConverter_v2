4ec48bf1e162e3ec1a710b271d4659461d89aece
Pavel Grigoriev <pavel.grigoriev.1979@yandex.ru>
2025-01-19 20:04:07 +0300

    Checkpoint: refactoring in process...

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <Windows.h>
 
-constexpr int MAX_NUMBER{1000000000};   // a number, before which numbers can be converted
-constexpr int power_of_ten{3};
+constexpr int max_number{1000000000};   // a number, before which numbers can be converted
+constexpr int position_calc{3};
 constexpr int number_ten{10};
 
 const char* units[] {"\b", "один", "два", "три", "четыре", "пять",
@@ -28,6 +28,10 @@ const char* million_default{"миллионов"};
 const char* million_var1{"миллион"};
 const char* million_var2{"миллиона"};
 
+const char* ruble_default{"рублей"};
+const char* ruble_var1{"рубль"};
+const char* ruble_var2{"рубля"};
+
 const char* thousands[]{thousand_default, thousand_var1, thousand_var2,
     thousand_var2, thousand_var2, thousand_default,
     thousand_default, thousand_default, thousand_default,
@@ -38,40 +42,149 @@ const char* millions[]{million_default, million_var1, million_var2,
     million_default, million_default, million_default,
     million_default};
 
+const char* rubles[]{ruble_default, ruble_var1, ruble_var2,
+    ruble_var2, ruble_var2, ruble_default,
+    ruble_default, ruble_default, ruble_default,
+    ruble_default};
+
 
 int getNumberLength(int number)
 {
     int length{0};
 
-    for(; number != 0; number /= 10)
+    while(number != 0)
     {
         ++length;
+        number /= number_ten;
     }
 
     return length;
 }
 
 
-int getInput()
+int getNumber()
 {
-    int input{0};
+    int num{0};
     std::cout << "Введите положительное число: ";
-    std::cin >> input;
+    std::cin >> num;
 
-    return input;
+    return num;
 }
 
 
-void numberToArray(int* &arr, int num)
+int* numberToArray(int num, int length)
 {
-    int length{getNumberLength(num)};
-    arr = new int[static_cast<std::size_t>(length)]{};
+    int* arr{new int[static_cast<std::size_t>(length)]{}};
 
-    for(int i{length - 1}; i >= 0; --i)
+    for(int i{0}; i < length; ++i)
     {
-        *(arr + i) = num % 10;
-        num /= 10;
+        arr[i] = num % number_ten;
+        num /= number_ten;
     }
+
+    return arr;
+}
+
+
+const char* convertNumberToWord(int digit, int numIndex, int position, bool& isNteen)
+{
+    const char* result;
+
+    switch(position)
+    {
+    case 0:
+        if(isNteen)
+            result = teens[digit];
+        else if(numIndex == 3)
+            result = units_alt[digit];
+        else
+            result = units[digit];
+        break;
+    case 1:
+        result = tens[digit];
+        isNteen = (digit == 1);
+        break;
+    case 2:
+        result = hundreds[digit];
+        break;
+    default:
+        result = "UNEXPECTED!";
+    }
+
+    return result;
+}
+
+
+void printConvertedNumber(int num)
+{
+    int numberLength{getNumberLength(num)};
+    int* numArr{numberToArray(num, numberLength)};
+    bool isNteen{false};
+
+    for(int numIndex{numberLength - 1}; numIndex >= 0; --numIndex)
+    {
+        int digit{numArr[numIndex]};
+        int position{numIndex % position_calc};
+        const char * word{};
+
+//        switch(position)
+//        {
+//        case 0:
+//            if(numIndex == 3)
+//                word = units_alt[digit];
+//            else
+//                word = units[digit];
+//            break;
+//        case 1:
+//            word = tens[digit];
+//            if(digit == 1)
+//            {
+//                --numIndex;
+//                digit = numArr[numIndex];
+//                position = numIndex % position_calc;
+//                word = teens[digit];
+//                isNteen = true;
+//            }
+//            break;
+//        case 2:
+//            word = hundreds[digit];
+//            break;
+//        }
+
+        word = convertNumberToWord(digit,numIndex, position, isNteen);
+        std::cout << word << ' ';
+
+        if(position == 0)
+        {
+            switch(numIndex)
+            {
+            case 0:
+                if(isNteen)
+                    word = ruble_default;
+                else
+                    word = rubles[digit];
+                break;
+            case 3:
+                if(isNteen)
+                    word = thousand_default;
+                else
+                    word = thousands[digit];
+                break;
+            case 6:
+                if(isNteen)
+                    word = million_default;
+                else
+                    word = millions[digit];
+                break;
+            }
+
+            isNteen = false;
+
+            std::cout << word << ' ';
+        }
+    }
+
+    delete[] numArr;
 }
 
 
@@ -80,25 +193,11 @@ int main()
     SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8);
 
-    int input{getInput()};
-    int numberLength{getNumberLength(input)};
-    int* numArr{nullptr};
+    int input{getNumber()};
 
-    numberToArray(numArr, input);
-
-//    for(int i{numberLength - 1}; i >= 0; --i)
-//    {
-//        *(numArr + i) = input % 10;
-//        input /= 10;
-//    }
-
-    for(int i{0}; i < numberLength; ++i)
-    {
-        std::cout << *(numArr + i) << ' ';
-    }
+    printConvertedNumber(input);
 
     std::cout << '\n';
 
-    delete[] numArr;
     return 0;
 }
